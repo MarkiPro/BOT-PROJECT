81852e052fee767647bfb4cc15f6aa5fb7ec7e66
MarkiPro <marko.pasic.com@gmail.com>
2020-05-09 20:16:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,28 +12,18 @@ async def on_ready():
 @client.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
-    if member.has_permissions(ban_members=True, kick_members=True):
-        ctx.send(f"I do not have permission to kick {member.display_name}!")
-        return
-    else:
-        pass
-        async with ctx.typing():
-            await member.send(f"You have been kicked in **{ctx.guild}** for `{reason}`")
-        await member.kick(reason=reason)
-        await ctx.send(f"{member.mention} has been kicked for: `{reason}`")
+    async with ctx.typing():
+        await member.send(f"You have been kicked in **{ctx.guild}** for `{reason}`")
+    await member.kick(reason=reason)
+    await ctx.send(f"{member.mention} has been kicked for: `{reason}`")
 
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
-    if member.has_permissions(ban_members=True, kick_members=True):
-        ctx.send(f"I do not have permission to ban {member.display_name}!")
-        return
-    else:
-        pass
-        async with ctx.typing():
-            await member.send(f"You have been banned in **{ctx.guild}** for `{reason}`")
-        await member.ban(reason=reason)
-        await ctx.send(f"{member.mention} has been banned for: `{reason}`")
+    async with ctx.typing():
+        await member.send(f"You have been banned in **{ctx.guild}** for `{reason}`")
+    await member.ban(reason=reason)
+    await ctx.send(f"{member.mention} has been banned for: `{reason}`")
 
 @client.command()
 @commands.has_permissions(ban_members=True)
