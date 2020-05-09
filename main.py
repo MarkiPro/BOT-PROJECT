6302d68e9e510c88d80fe5b5bfa07dc135d4c652
Sahaj <sahajemast@gmail.com>
2020-05-09 16:10:28 +0530

    Added Kick Command, and Error Handling

--- main.py
+++ main.py
@@ -14,4 +14,18 @@ async def on_ready():
 async def help(ctx):
     await ctx.send(f"NOOB {ctx.message.author.mention}")
 
+@client.command()
+@commands.has_permissions(KICK_MEMBERS=True)
+async def kick(ctx, member: discord.Member, *, reason):
+    await member.kick(reason=reason)
+    await ctx.send(f"{member.mention} has been banned for {reason}")
+
+@client.event
+async def on_command_error(self, ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You are missing permissions to perform this command")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please provide all the required arguments. Please run r!help.")
+
+
 client.run(token)
