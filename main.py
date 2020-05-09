d41bef81f3db7db41ad721e23fe947683373cd67
MarkiPro <marko.pasic.com@gmail.com>
2020-05-09 16:13:25 +0200

    bantes

--- main.py
+++ main.py
@@ -23,8 +23,20 @@ async def on_command_error(ctx, error):
         await ctx.send("You are missing permissions to perform this command")
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please provide all the required arguments. Please run r!help.")
-    if isinstance(error, commands.CommandInvokeError):
-        await ctx.send("Something went wrong.")
 
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    async with ctx.typing():
+        await member.send(f"You have been banned in **{ctx.guild}** for \`{reason}\`")
+    await member.ban(reason=reason)
+    await ctx.send(f"{member.mention} has been banned for: `{reason}`")
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to use this command!")
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please provide all the require arguments, use `r!help <command>`!")
 
 client.run(token)
