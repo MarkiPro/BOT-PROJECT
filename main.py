a6af00f72dc07b44597aef31e0fe8b94ac692094
MarkiPro <marko.pasic.com@gmail.com>
2020-05-09 23:48:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 import os
+from discord import user
 
+embed = discord.Embed
+embed2 = discord.Embed
 token = os.environ['TOKEN']
 client = commands.Bot(command_prefix='r!')
 
@@ -11,7 +14,11 @@ async def on_ready():
 
 @client.command()
 @commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
+async def kick(ctx, member: discord.Member, *, reason=NotImplemented):
+    embed.color(embed, "#07ff00")
+    embed.title(embed, "SUCCESS")
+    embed.description(f"***\:white_check_mark: {user.display_name} has been unbanned!***")
+    embed.timestamp(ctx.timestamp)
     async with ctx.typing():
         await member.send(f"You have been kicked in **{ctx.guild}** for `{reason}`")
     await member.kick(reason=reason)
@@ -20,14 +27,22 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
+    embed.color(embed, "#07ff00")
+    embed.title(embed, "SUCCESS")
+    embed.description(f"***\:white_check_mark: {member.display_name} has been banned for:*** `{reason}`")
+    embed.timestamp(ctx.timestamp)
     async with ctx.typing():
-        await member.send(f"You have been banned in **{ctx.guild}** for `{reason}`")
+        await member.send()
     await member.ban(reason=reason)
-    await ctx.send(f"{member.mention} has been banned for: `{reason}`")
+    await ctx.send(embed)
 
 @client.command()
 @commands.has_permissions(ban_members=True)
 async def unban(self,ctx, *, user):
+    embed.color(embed, "#07ff00")
+    embed.title(embed, "SUCCESS")
+    embed.description(f"***\:white_check_mark: {user.display_name} has been unbanned!***")
+    embed.timestamp(ctx.timestamp)
     ban_list = await ctx.guild.bans()
     user_name, user_discriminator = user.split('#')
 
@@ -35,14 +50,22 @@ async def unban(self,ctx, *, user):
         user = ban_entry.user
         if(user.name, user.discriminator) == (user_name,user_discriminator):
             await ctx.guild.unban(user)
-            await ctx.send(f"{user.name}#{user.discriminator} has been unbanned!")
+            await ctx.send(embed)
             return
 
 @client.event
 async def on_command_error(ctx, error):
+    embed.color(embed, "#07ff00")
+    embed.title(embed, "ERROR")
+    embed.description(f"***\:warning: You do not have permission to use this command!***")
+    embed.timestamp(ctx.timestamp)
+    embed2.color(embed, "#07ff00")
+    embed2.title(embed, "ERROR")
+    embed2.description(f"***\:warning: Please provide all the require arguments, use `r!help <command>` for more information!***")
+    embed2.timestamp(ctx.timestamp)
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have permission to use this command!")
+        await ctx.send(embed)
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Please provide all the require arguments, use `r!help <command>`!")
+        await ctx.send(embed2)
 
 client.run(token)
