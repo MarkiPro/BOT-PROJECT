428470ffbed158a17bdce5ffeb0398bd43504b03
Sahaj <sahajemast@gmail.com>
2020-05-09 16:11:04 +0530

    Readded default help command, since it is very useful

--- main.py
+++ main.py
@@ -4,15 +4,10 @@ import os
 
 token = os.environ['TOKEN']
 client = commands.Bot(command_prefix='r!')
-client.remove_command('help')
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user.tag}")
-
-@client.command()
-async def help(ctx):
-    await ctx.send(f"NOOB {ctx.message.author.mention}")
 
 @client.command()
 @commands.has_permissions(KICK_MEMBERS=True)
