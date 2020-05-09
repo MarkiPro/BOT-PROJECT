166fde4200530ece283e017af6119bf4b3f4f9e3
Sahaj <sahajemast@gmail.com>
2020-05-09 15:13:35 +0530

    Version 0.1: Converted to Discord.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord.py
+from discord.py import commands
+import os
+
+token = os.environ['TOKEN']
+client = commands.Bot(command_prefix='r!')
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user.tag}")
+
+@client.command()
+async def help(ctx):
+    await ctx.send(f"NOOB {ctx.message.author.mention}")
+
+client.run(token)
