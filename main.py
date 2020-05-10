dc5bc393b6960dff06f273de37dde199cf93e27c
MarkiPro <marko.pasic.com@gmail.com>
2020-05-10 17:14:58 +0200

    embeds

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ client = commands.Bot(command_prefix='r!')
 async def on_ready():
     print(f"Logged in as {client.user.tag}")
 
+client.remove_command('help')
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title="COMMANDS:", 
+        description="""
+        {prefix}help - shows this message;
+        {prefix}clear - clears messages above;
+        {prefix}kick - kicks a member;
+        {prefix}ban - bans a member;
+        {prefix}unban - unbans a member;""",
+        color="#0076ff")
+    await ctx.channel.send(embed=embed)
+
 @client.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
@@ -31,6 +46,7 @@ async def ban(ctx, member: discord.Member, *, reason=None):
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount):
+    amount=0
     await ctx.channel.purge(limit=amount+1)
 
 @client.command()
@@ -48,10 +64,10 @@ async def unban(ctx, member, *, reason=None):
         if (user.name, user.discriminator) == (user_name, user_discriminator) or int(id) == user.id:
             await ctx.guild.unban(user, reason=reason)
             await ctx.send(f"{user.name}#{user.discriminator} has been unbanned")
-"""@client.event
+@client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.send(f"***\:warning: You do not have permission to use this command!***")
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f"***\:warning: Please provide all the require arguments, use `r!help <command>` for more information!***")"""
+        await ctx.send(f"***\:warning: Please provide all the require arguments, use `r!help <command>` for more information!***")
 client.run(token)
