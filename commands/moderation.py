f120dea5fb8c24d8099bb960d6bd07fe9fc40d32
MarkiPro <marko.pasic.com@gmail.com>
2020-05-11 17:04:09 +0200

    Update moderation.py

--- commands/moderation.py
+++ commands/moderation.py
@@ -6,7 +6,7 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-@client.command()
+@commands.command()
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     embed1 = discord.Embed(
@@ -22,28 +22,28 @@ async def kick(ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(embed=embed1)
 
-@client.command()
+@commands.command()
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=0):
     await ctx.channel.purge(limit=amount+1)
 
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
-        embed1 = discord.Embed(
-            title="**SUCCESS**",
-            description=f"***:white_check_mark: *** {member.display_name} *** has been banned for: `{reason}`***",
-            color=0x00fa00)
-        embed2 = discord.Embed(
-            title="**NOTIFICATION**",
-            description=f":bell: *You have been banned in **{ctx.guild}** for:* `{reason}`",
-            color=0x0064ff)
-        async with ctx.typing():
-            await member.ban(reason=reason)
-            await ctx.send(embed=embed1)
-            await member.send(embed=embed2)
+@commands.command()
+@commands.has_permissions(ban_members=True)
+async def ban(self, ctx, member: discord.Member, *, reason=None):
+    embed1 = discord.Embed(
+        title="**SUCCESS**",
+        description=f"***:white_check_mark: *** {member.display_name} *** has been banned for: `{reason}`***",
+        color=0x00fa00)
+    embed2 = discord.Embed(
+        title="**NOTIFICATION**",
+        description=f":bell: *You have been banned in **{ctx.guild}** for:* `{reason}`",
+        color=0x0064ff)
+    async with ctx.typing():
+        await member.ban(reason=reason)
+        await ctx.send(embed=embed1)
+        await member.send(embed=embed2)
 
-@client.command()
+@commands.command()
 @commands.has_permissions(ban_members=True)
 async def unban(ctx, member, *, reason=None):
     ban_list = await ctx.guild.bans()
