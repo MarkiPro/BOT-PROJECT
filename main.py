0be83380c2c82f9372426b2d06c358ee63659911
MarkiPro <marko.pasic.com@gmail.com>
2020-05-11 16:18:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
     bot = commands.Bot(help_command=EmbedHelpCommand())
     """
     # Set the embed colour here
-    COLOUR = discord.Colour.blue()
+    COLOUR = discord.Colour.DodgerBlue()
  
     def get_ending_note(self):
         return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
@@ -67,52 +67,11 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
         await self.get_destination().send(embed=embed)
 
 token = os.environ['TOKEN']
-client = commands.Bot(command_prefix='rm!', help_command=EmbedHelpCommand())
+client = commands.Bot(command_prefix='rm!', help_command=EmbedHelpCommand(), case_insensitive=True)
 
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user.tag}")
-
-@client.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    embed1 = discord.Embed(
-        title="**SUCCESS**", 
-        description=f"***:white_check_mark: *** {member.display_name} *** has been kicked for: `{reason}`***",
-        color=0x00fa00)
-    embed2 = discord.Embed(
-        title="**NOTIFICATION**", 
-        description=f":bell: *You have been kicked in **{ctx.guild}** for:* `{reason}`",
-        color=0x0064ff)
-    async with ctx.typing():
-        await member.send(embed=embed2)
-        await member.kick(reason=reason)
-        await ctx.send(embed=embed1)
-
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, amount=0):
-    await ctx.channel.purge(limit=amount+1)
-
-@client.command()
-@commands.has_permissions(ban_members=True)
-async def unban(ctx, member, *, reason=None):
-    ban_list = await ctx.guild.bans()
-    for ban_entry in ban_list:
-        user = ban_entry.user
-        id = member
-        embed1 = discord.Embed(
-        title="**SUCCESS**", 
-        description=f"***:white_check_mark: *** {user.display_name} *** has been unbanned for: `{reason}`***",
-        color=0x00fa00)
-        try:
-            user_name, user_discriminator = member.split('#')
-        except ValueError:
-            user_name = ''
-            user_discriminator = ''
-        if (user.name, user.discriminator) == (user_name, user_discriminator) or int(id) == user.id:
-            await ctx.guild.unban(user, reason=reason)
-            await ctx.send(embed=embed1)
 
 for file in os.listdir('commands/'):
     if file.endswith('.py'):
