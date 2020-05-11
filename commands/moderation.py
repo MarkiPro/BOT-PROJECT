28f94c0d241cc62b03a2a4be93bee52fd8070b06
MarkiPro <marko.pasic.com@gmail.com>
2020-05-11 16:18:58 +0200

    Update moderation.py

--- commands/moderation.py
+++ commands/moderation.py
@@ -6,6 +6,27 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    embed1 = discord.Embed(
+        title="**SUCCESS**", 
+        description=f"***:white_check_mark: *** {member.display_name} *** has been kicked for: `{reason}`***",
+        color=0x00fa00)
+    embed2 = discord.Embed(
+        title="**NOTIFICATION**", 
+        description=f":bell: *You have been kicked in **{ctx.guild}** for:* `{reason}`",
+        color=0x0064ff)
+    async with ctx.typing():
+        await member.send(embed=embed2)
+        await member.kick(reason=reason)
+        await ctx.send(embed=embed1)
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=0):
+    await ctx.channel.purge(limit=amount+1)
+
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
@@ -21,6 +42,26 @@ class Moderation(commands.Cog):
             await member.ban(reason=reason)
             await ctx.send(embed=embed1)
             await member.send(embed=embed2)
+
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, member, *, reason=None):
+    ban_list = await ctx.guild.bans()
+    for ban_entry in ban_list:
+        user = ban_entry.user
+        id = member
+        embed1 = discord.Embed(
+        title="**SUCCESS**", 
+        description=f"***:white_check_mark: *** {user.display_name} *** has been unbanned for: `{reason}`***",
+        color=0x00fa00)
+        try:
+            user_name, user_discriminator = member.split('#')
+        except ValueError:
+            user_name = ''
+            user_discriminator = ''
+        if (user.name, user.discriminator) == (user_name, user_discriminator) or int(id) == user.id:
+            await ctx.guild.unban(user, reason=reason)
+            await ctx.send(embed=embed1)
 
 def setup(client):
     client.add_cog(Moderation(client))
