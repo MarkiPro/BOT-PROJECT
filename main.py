61177d45df6b83def5f1455330f6a1bb21e08257
MarkiPro <marko.pasic.com@gmail.com>
2020-05-11 22:58:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,38 +15,8 @@ async def on_ready():
 @client.command()
 async def help(ctx, *, commandArg=None):
     prefix = await client.get_prefix(ctx.message)
-    if(str(commandArg)) == ("ban"):
-        embed = discord.Embed(
-            title="**COMMAND**", 
-            description=f"`{prefix[0]}ban <user> [reason]` - This is the correct usage of the ban command, reason is by default set to `None`.",
-            color=0x0064ff)
-        await ctx.send(embed=embed)
-    if(str(commandArg)) == ("kick"):
-        embed = discord.Embed(
-            title="**COMMAND**", 
-            description=f"`{prefix[0]}kick <user> [reason]` - This is the correct usage of the kick command, reason is by default set to `None`.",
-            color=0x0064ff)
-        await ctx.send(embed=embed)
-    if(str(commandArg)) == ("clear"):
-        embed = discord.Embed(
-            title="**COMMAND**", 
-            description=f"`{prefix[0]}clear <amount>` - This is the correct usage of the clear command, amount is by default set to `0`, so it won't delete any other message apart from yours.",
-            color=0x0064ff)
-        await ctx.send(embed=embed)
-    if(str(commandArg)) == ("unban"):
-        embed = discord.Embed(
-            title="**COMMAND**", 
-            description=f"`{prefix[0]}unban <user> [reason]` - This is the correct usage of the unban command, by default reason is set to `None`.",
-            color=0x0064ff)
-        await ctx.send(embed=embed)
-    if(str(commandArg)) == ("help"):
-        embed = discord.Embed(
-            title="**COMMAND**", 
-            description=f"`{prefix[0]}help [command]` - This is the correct usage of the help command. This command will inform you about any command that you'd like to, or all the commands, by leaving the command argument empty.",
-            color=0x0064ff)
-        await ctx.send(embed=embed)
-    if(commandArg):
-        embed = discord.Embed(
+    if(not commandArg):
+        embed6 = discord.Embed(
             title="**COMMANDS:**", 
             description=f"""**--INFORMATIVE--**
             
@@ -61,7 +31,37 @@ async def help(ctx, *, commandArg=None):
 
                             """,
             color=0x0064ff)
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed6)
+    if(str(commandArg)) == ("ban"):
+        embed1 = discord.Embed(
+            title="**COMMAND**", 
+            description=f"`{prefix[0]}ban <user> [reason]` - This is the correct usage of the ban command, reason is by default set to `None`.",
+            color=0x0064ff)
+        await ctx.send(embed=embed1)
+    if(str(commandArg)) == ("kick"):
+        embed2 = discord.Embed(
+            title="**COMMAND**", 
+            description=f"`{prefix[0]}kick <user> [reason]` - This is the correct usage of the kick command, reason is by default set to `None`.",
+            color=0x0064ff)
+        await ctx.send(embed=embed2)
+    if(str(commandArg)) == ("clear"):
+        embed3 = discord.Embed(
+            title="**COMMAND**", 
+            description=f"`{prefix[0]}clear <amount>` - This is the correct usage of the clear command, amount is by default set to `0`, so it won't delete any other message apart from yours.",
+            color=0x0064ff)
+        await ctx.send(embed=embed3)
+    if(str(commandArg)) == ("unban"):
+        embed4 = discord.Embed(
+            title="**COMMAND**", 
+            description=f"`{prefix[0]}unban <user> [reason]` - This is the correct usage of the unban command, by default reason is set to `None`.",
+            color=0x0064ff)
+        await ctx.send(embed=embed4)
+    if(str(commandArg)) == ("help"):
+        embed5 = discord.Embed(
+            title="**COMMAND**", 
+            description=f"`{prefix[0]}help [command]` - This is the correct usage of the help command. This command will inform you about any command that you'd like to, or all the commands, by leaving the command argument empty.",
+            color=0x0064ff)
+        await ctx.send(embed=embed5)
 
 @client.command()
 @commands.has_permissions(kick_members=True)
