a0ecc30f73f4781a5c7e55fde347a2033329ea76
MarkiPro <marko.pasic.com@gmail.com>
2020-05-21 10:39:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mysql.connector
 import datetime
 
 token = os.environ['TOKEN']
-client = commands.Bot(command_prefix='rm!', case_insensitive=True)
+client = commands.Bot(command_prefix='r!', case_insensitive=True)
 @client.event
 async def on_ready():
     print(f"Ready")
@@ -100,33 +100,37 @@ async def help(ctx, *, commandArg=None):
 
 @client.command()
 @commands.cooldown(1, 60, commands.BucketType.member)
-async def credits():
+async def credits(ctx):
+    user_convertor = commands.UserConvertor()
+    markipro = await user_convertor.convert(ctx, "438333007036678155")
+    malware = await user_convertor.convert(ctx, "466591581286170624")
     suggestedEmbed = discord.Embed(
         title=f"**CREDITS**",
-        description=f"""
-        This bot was originally made by `MarkiPro#0825`.
-        HUUUUUUGE Thank you to `Malware#1234` for being of big help with setting a big chunk of this bot up.""",
+        description=f"{markipro} and {malware} made the bot.",
         color=0x0064ff,
         timestamp=datetime.datetime.now(tz=None)
         )
+    await ctx.send(embed=suggestedEmbed)
 
 @client.command()
-@commands.cooldown(1, 3600, commands.BucketType.member)
-async def suggest(ctx):
+@commands.cooldown(1, 86400, commands.BucketType.member)
+async def post(ctx):
     startedEmbed = discord.Embed(
-        title="**SUGGESTION SETUP**",
-        description="Please continue in dms.",
+        title="**POST SETUP**",
+        description="Please continue the setup in dms",
         color=0x0064ff,
         timestamp=datetime.datetime.now(tz=None)
-        )
-    furstEmbed = discord.Embed(
-        title="**SUGGESTION SETUP**",
-        description="What would you like to name your suggestion?",
+    )
+    firstEmbed = discord.Embed(
+        title="**POST SETUP**",
+        description=f"""What would you like to do?
+                        
+                        `hire`, `get hired`, `sell creation`, `advertise game`""",
         color=0x0064ff,
         timestamp=datetime.datetime.now(tz=None)
-        )
+    )
     await ctx.send(embed=startedEmbed)
-    await ctx.author.send(embed=furstEmbed)
+    await ctx.author.send(embed=firstEmbed)
     def check(m):
         if isinstance(m.channel, discord.DMChannel):
             if m.author == ctx.author:
@@ -135,53 +139,257 @@ async def suggest(ctx):
                 return False
         else:
             return False
-    title_message = await client.wait_for('message', check=check, timeout=500)
-    title = title_message.content
-    startEmbed = discord.Embed(
-        title="**SUGGESTION SETUP**",
-        description="Please write down your suggestion in detail.",
-        color=0x0064ff,
-        timestamp=datetime.datetime.now(tz=None)
+    category_message = await client.wait_for('message', check=check, timeout=960)
+    category = category_message.content
+    if(category == "hire"):
+        firstHireEmbed = discord.Embed(
+            title="**HIRING SETUP**",
+            description="Please go as much in detail about your hiring post.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
         )
-    await ctx.author.send(embed=startEmbed)
-    body_message = await client.wait_for('message', check=check, timeout=500)
-    body = body_message.content
-    suggestedEmbed2 = discord.Embed(
-        title=f"**FINISHED PRODUCT**",
-        description=f"""*Say `done` to post.*""",
-        color=0x0064ff,
-        timestamp=datetime.datetime.now(tz=None)
+        message_details = await client.wait_for('message', check=check, timeout=960)
+        details = message_details.content
+        secondHireEmbed = discord.Embed(
+            title="**HIRING SETUP**",
+            description="Who are you looking to hire? E.g. scripter, builder etc.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
         )
-    suggestedEmbed1 = discord.Embed(
-        title=f"**{title}**",
-        description=f"{body}",
-        color=0x0064ff,
-        timestamp=datetime.datetime.now(tz=None)
+        lookingFor_msg = await client.wait_for('message', check=check, timeout=960)
+        lookingFor = lookingFor_msg
+        thirdHireEmbed = discord.Embed(
+            title="**HIRING SETUP**",
+            description="""How would you like to pay?
+                            `percentage`, `robux`, `USD`, `other`""",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
         )
-    suggestedEmbed1.set_footer(text=f"by: {ctx.author}")
-    await ctx.author.send(embed=suggestedEmbed2)
-    await ctx.author.send(embed=suggestedEmbed1)
-    body_message2 = await client.wait_for('message', check=check, timeout=500)
-    body2 = body_message2.content
-    if(body2 == "done"):
-        finalEmbed = discord.Embed(
+        message_body1 = await client.wait_for('message', check=check, timeout=960)
+        body1 = message_body1.content
+        if(body1 == "percentage"):
+            fourthHireEmbed = discord.Embed(
+                title="**HIRING SETUP**",
+                description="How big of a percentage are you willing to give?",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None),
+                set_footer="Timeout on this message is "
+            )
+            percentage_amount = await client.wait_for('message', check=check, timeout=960)
+
+@client.command()
+@commands.cooldown(1, 3600, commands.BucketType.member)
+async def suggest(ctx):
+    if ctx.guild.name == "RoDev's":
+        suggestionsChannel = client.get_channel(id=711307176899248149)
+        startedEmbed = discord.Embed(
             title="**SUGGESTION SETUP**",
-            description="Your suggestion has been posted.",
+            description="Please continue the setup in dms.",
             color=0x0064ff,
             timestamp=datetime.datetime.now(tz=None)
             )
-        await ctx.author.send(embed=finalEmbed)
-        suggestionsChannel = client.get_channel(id=711307176899248149)
-        suggestedEmbed = discord.Embed(
+        furstEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="What would you like to name your suggestion?",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.send(embed=startedEmbed)
+        await ctx.author.send(embed=furstEmbed)
+        def check(m):
+            if isinstance(m.channel, discord.DMChannel):
+                if m.author == ctx.author:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        title_message = await client.wait_for('message', check=check, timeout=1000)
+        title = title_message.content
+        startEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="Please write down your suggestion in detail.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.author.send(embed=startEmbed)
+        body_message = await client.wait_for('message', check=check, timeout=1000)
+        body = body_message.content
+        suggestedEmbed2 = discord.Embed(
+            title=f"**FINISHED PRODUCT**",
+            description=f"""*Say `done` to post.*""",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        suggestedEmbed1 = discord.Embed(
             title=f"**{title}**",
             description=f"{body}",
             color=0x0064ff,
             timestamp=datetime.datetime.now(tz=None)
             )
-        suggestedEmbed.set_footer(text=f"by: {ctx.author}")
-        sent = await suggestionsChannel.send(embed=suggestedEmbed)
-        await sent.add_reaction('👍')
-        await sent.add_reaction('👎')
+        suggestedEmbed1.set_footer(text=f"by: {ctx.author}")
+        await ctx.author.send(embed=suggestedEmbed2)
+        await ctx.author.send(embed=suggestedEmbed1)
+        body_message2 = await client.wait_for('message', check=check, timeout=1000)
+        body2 = body_message2.content
+        if(body2 == "done"):
+            finalEmbed = discord.Embed(
+                title="**SUGGESTION SETUP**",
+                description="Your suggestion has been posted.",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            await ctx.author.send(embed=finalEmbed)
+            suggestedEmbed = discord.Embed(
+                title=f"**{title}**",
+                description=f"{body}",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            suggestedEmbed.set_footer(text=f"by: {ctx.author}")
+            sent = await suggestionsChannel.send(embed=suggestedEmbed)
+            await sent.add_reaction('👍')
+            await sent.add_reaction('👎')
+    if ctx.guild.name == "Content Creators":
+        suggestionsChannel = client.get_channel(id=711307176899248149)
+        startedEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="Please continue the setup in dms.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        furstEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="What would you like to name your suggestion?",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.send(embed=startedEmbed)
+        await ctx.author.send(embed=furstEmbed)
+        def check(m):
+            if isinstance(m.channel, discord.DMChannel):
+                if m.author == ctx.author:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        title_message = await client.wait_for('message', check=check, timeout=1000)
+        title = title_message.content
+        startEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="Please write down your suggestion in detail.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.author.send(embed=startEmbed)
+        body_message = await client.wait_for('message', check=check, timeout=1000)
+        body = body_message.content
+        suggestedEmbed2 = discord.Embed(
+            title=f"**FINISHED PRODUCT**",
+            description=f"""*Say `done` to post.*""",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        suggestedEmbed1 = discord.Embed(
+            title=f"**{title}**",
+            description=f"{body}",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        suggestedEmbed1.set_footer(text=f"by: {ctx.author}")
+        await ctx.author.send(embed=suggestedEmbed2)
+        await ctx.author.send(embed=suggestedEmbed1)
+        body_message2 = await client.wait_for('message', check=check, timeout=1000)
+        body2 = body_message2.content
+        if(body2 == "done"):
+            finalEmbed = discord.Embed(
+                title="**SUGGESTION SETUP**",
+                description="Your suggestion has been posted.",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            await ctx.author.send(embed=finalEmbed)
+            suggestedEmbed = discord.Embed(
+                title=f"**{title}**",
+                description=f"{body}",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            suggestedEmbed.set_footer(text=f"by: {ctx.author}")
+            sent = await suggestionsChannel.send(embed=suggestedEmbed)
+            await sent.add_reaction('👍')
+            await sent.add_reaction('👎')
+    if ctx.guild.name == "RoDev's":
+        suggestionsChannel = client.get_channel(id=711307176899248149)
+        startedEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="Please continue the setup in dms.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        furstEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="What would you like to name your suggestion?",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.send(embed=startedEmbed)
+        await ctx.author.send(embed=furstEmbed)
+        def check(m):
+            if isinstance(m.channel, discord.DMChannel):
+                if m.author == ctx.author:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        title_message = await client.wait_for('message', check=check, timeout=1000)
+        title = title_message.content
+        startEmbed = discord.Embed(
+            title="**SUGGESTION SETUP**",
+            description="Please write down your suggestion in detail.",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        await ctx.author.send(embed=startEmbed)
+        body_message = await client.wait_for('message', check=check, timeout=1000)
+        body = body_message.content
+        suggestedEmbed2 = discord.Embed(
+            title=f"**FINISHED PRODUCT**",
+            description=f"""*Say `done` to post.*""",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        suggestedEmbed1 = discord.Embed(
+            title=f"**{title}**",
+            description=f"{body}",
+            color=0x0064ff,
+            timestamp=datetime.datetime.now(tz=None)
+            )
+        suggestedEmbed1.set_footer(text=f"by: {ctx.author}")
+        await ctx.author.send(embed=suggestedEmbed2)
+        await ctx.author.send(embed=suggestedEmbed1)
+        body_message2 = await client.wait_for('message', check=check, timeout=1000)
+        body2 = body_message2.content
+        if(body2 == "done"):
+            finalEmbed = discord.Embed(
+                title="**SUGGESTION SETUP**",
+                description="Your suggestion has been posted.",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            await ctx.author.send(embed=finalEmbed)
+            suggestedEmbed = discord.Embed(
+                title=f"**{title}**",
+                description=f"{body}",
+                color=0x0064ff,
+                timestamp=datetime.datetime.now(tz=None)
+                )
+            suggestedEmbed.set_footer(text=f"by: {ctx.author}")
+            sent = await suggestionsChannel.send(embed=suggestedEmbed)
+            await sent.add_reaction('👍')
+            await sent.add_reaction('👎')
 @client.command()
 @commands.has_permissions(manage_roles=True)
 @commands.cooldown(1, 5, commands.BucketType.member)
@@ -347,10 +555,10 @@ async def unban(ctx, member, *, reason=None):
 @commands.cooldown(1, 5, commands.BucketType.member)
 async def warn(ctx, member: discord.Member, *, reason):
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='',
-        database='discord-py-tutorial'
+        host="us-cdbr-east-06.cleardb.net",
+        user="baba29035f4254",
+        passwd="8a63c86d",
+        database="heroku_daa9f1b493ff319"
     )
     cursor = connection.cursor()
     cursor.execute("INSERT INTO warns VALUES (%s, %s, %s, %s, %s)", (None, ctx.guild.id, member.id, reason, ctx.author.id))
@@ -360,10 +568,10 @@ async def warn(ctx, member: discord.Member, *, reason):
 @client.command()
 async def warnings(ctx, member: discord.Member):
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='',
-        database='discord-py-tutorial'
+        host="us-cdbr-east-06.cleardb.net",
+        user="baba29035f4254",
+        passwd="8a63c86d",
+        database="heroku_daa9f1b493ff319"
     )
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM warns WHERE user_id = %s AND guild_id = %s", (member.id, ctx.guild.id))
@@ -378,10 +586,10 @@ async def warnings(ctx, member: discord.Member):
 @client.command()
 async def removewarn(ctx, *, id):
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='',
-        database='discord-py-tutorial'
+        host="us-cdbr-east-06.cleardb.net",
+        user="baba29035f4254",
+        passwd="8a63c86d",
+        database="heroku_daa9f1b493ff319"
     )
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM warns WHERE id = %s", (int(id), ))
@@ -415,7 +623,8 @@ async def on_command_error(ctx, error):
         )
     embed3 = discord.Embed(
         title="**ERROR**", 
-        description=f"***:no_entry_sign: The command is on a cooldown, please do not rush it.***",
+        description=f"""***:no_entry_sign: The command is on a cooldown, please do not rush it.
+                            cooldown for this command is {error.cooldown.per}, and you have {error.retry_after} left***""",
         color=0xff0000,
         timestamp=datetime.datetime.now(tz=None)
         )
